090480e686995010cd0f4a96a2a03b94f4339a6c
Taoudi <Taoudi128@hotmail.com>
2019-12-20 00:55:45 +0100

    hm

--- ID1354/sem3/classes/recept/Util/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace recept\Util;
-
-class constants {
-   const LOGIN_CONTR_KEY = 'logincontr';
-}
--- ID2216/README.md
+++
@@ -1,2 +0,0 @@
-# ID2216
-Course Project for Mobile Development (ID2216 KTH) with Yujue Wang and Yulian Luo
